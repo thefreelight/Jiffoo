fe01b07f5e06ad9aabe5b0154ca70c76d41ba086
simo chen <chent555888@gmail.com>
2026-09-21 13:51:22 +0800

    再次清理干净

--- stop-jiffoo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-ENV_FILE="${PWD}/.env.production.local"
-COMPOSE_FILE="${PWD}/docker-compose.prod.yml"
-
-docker compose --env-file "${ENV_FILE}" -f "${COMPOSE_FILE}" down
-echo "Stopped Jiffoo services."
